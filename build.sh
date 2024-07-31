0eec5b9dcb34052ecf9f55c25f242d6259e9cb82
fedi-nabli <fedinabli@gmail.com>
2024-07-31 16:34:17 +0100

    Initial commit with setup

---
+++ build.sh
@@ -0,0 +1,4 @@
+aarch64-linux-gnu-gcc -c boot.S -o ./build/boot.o
+aarch64-linux-gnu-gcc -c main.S -o ./build/main.o
+aarch64-linux-gnu-ld -nostdlib -T link.lds -o ./bin/kernel ./build/boot.o ./build/main.o
+aarch64-linux-gnu-objcopy -O binary ./bin/kernel ./bin/kernel8.img
