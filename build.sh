abd6f3a612eb5139d347e2c5ec549901b59f6897
fedi-nabli <fedinabli@gmail.com>
2024-07-31 16:36:40 +0100

    Added library helper functions

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 aarch64-linux-gnu-gcc -c boot.S -o ./build/boot.o
 aarch64-linux-gnu-gcc -c main.S -o ./build/main.o
-aarch64-linux-gnu-ld -nostdlib -T link.lds -o ./bin/kernel ./build/boot.o ./build/main.o
+aarch64-linux-gnu-gcc -c lib.S -o ./build/lib.o
+aarch64-linux-gnu-ld -nostdlib -T link.lds -o ./bin/kernel ./build/boot.o ./build/main.o ./build/lib.o
 aarch64-linux-gnu-objcopy -O binary ./bin/kernel ./bin/kernel8.img
